5ad6ce2073bf0db427afade93cba3b7ddcbfbb63
michael-duke <michael.21duke@gmail.com>
2022-12-19 11:56:40 +0300

    Rename path as categories and transactions for better convinience

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root 'users#index'
+  
+  resources :users do
+    resources :groups, as: "categories" do 
+      resources :expenses, as: "transactions"
+    end
+  end
 end
