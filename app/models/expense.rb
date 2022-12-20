bdc8cf83e486913699af6837d52d28fafced65bf
michael-duke <michael.21duke@gmail.com>
2022-12-21 01:27:14 +0300

    Improve and fix tyops in expense models Implement total_transactions method in group model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,5 +5,5 @@ class Expense < ApplicationRecord
 
   belongs_to :author, class_name: 'User'
   has_many :group_expenses, dependent: :destroy
-  has_many :groups, through: :group_budgets, dependent: :destroy
+  has_many :groups, through: :group_expenses, dependent: :destroy
 end
